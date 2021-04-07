f8ad97ae32a56b504290d8f9f908a621f3d12982
Yeti Sno <yeti@yetiz.org>
2021-04-07 17:20:37 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kklab-com/goth-panic
+
+go 1.16
+
+require github.com/kklab-com/goth-kklogger v0.0.0-20210329135227-9693bea481eb
---
+++ panic.go
@@ -0,0 +1,25 @@
+package panic
+
+import (
+	kklogger "github.com/kklab-com/goth-kklogger"
+)
+
+func Call(f func(v interface{})) {
+	if v := recover(); v != nil {
+		f(v)
+	}
+}
+
+func Log() {
+	if v := recover(); v != nil {
+		switch t := v.(type) {
+		case error:
+			kklogger.ErrorJ("panic.Log", t.Error())
+		}
+	}
+}
+
+func Catch(main func(), panic func(v interface{})) {
+	defer Call(panic)
+	main()
+}
