5b20ee081cd06e142f6fe0e857f776f620141fd4
Yeti Sno <yeti@yetiz.org>
2021-04-07 18:15:26 +0800

    add log

--- panic.go
+++ panic.go
@@ -1,6 +1,11 @@
 package kkpanic
 
 import (
+	"bytes"
+	"encoding/json"
+	"runtime/debug"
+	"runtime/pprof"
+
 	kklogger "github.com/kklab-com/goth-kklogger"
 )
 
@@ -12,9 +17,15 @@ func Call(f func(v interface{})) {
 
 func Log() {
 	if v := recover(); v != nil {
+		buffer := &bytes.Buffer{}
+		pprof.Lookup("goroutine").WriteTo(buffer, 1)
 		switch t := v.(type) {
 		case error:
-			kklogger.ErrorJ("panic.Log", t.Error())
+			kklogger.ErrorJ("panic.Log", CaughtError{
+				Err:             t.Error(),
+				PanicCallStack:  string(debug.Stack()),
+				GoRoutineStacks: buffer.String(),
+			})
 		}
 	}
 }
@@ -22,4 +33,15 @@ func Log() {
 func Catch(main func(), panic func(v interface{})) {
 	defer Call(panic)
 	main()
+}
+
+type CaughtError struct {
+	Err             string `json:"error,omitempty"`
+	PanicCallStack  string `json:"panic_call_stack,omitempty"`
+	GoRoutineStacks string `json:"go_routine_stacks,omitempty"`
+}
+
+func (e *CaughtError) Error() string {
+	bs, _ := json.Marshal(e)
+	return string(bs)
 }
