0fbbccd74c9c8318ce1d1ec64c7f4ead4089c081
Yeti Sno <yeti@yetiz.org>
2021-04-15 15:15:34 +0800

    add interface

--- panic.go
+++ panic.go
@@ -9,69 +9,74 @@ import (
 	kklogger "github.com/kklab-com/goth-kklogger"
 )
 
-func Convert(v interface{}) *Caught {
+func Convert(v interface{}) Caught {
 	if v == nil {
 		return nil
 	}
 
 	buffer := &bytes.Buffer{}
 	pprof.Lookup("goroutine").WriteTo(buffer, 1)
-	c := &Caught{
+	c := &CaughtImpl{
 		Message:         v,
-		PanicCallStack:  string(debug.Stack()),
-		GoRoutineStacks: buffer.String(),
+		CallStack:       string(debug.Stack()),
+		GoroutineStacks: buffer.String(),
 	}
 
 	buffer.Reset()
 	return c
 }
 
-func Call(f func(r *Caught)) {
+func Call(f func(r Caught)) {
 	if v := recover(); v != nil {
 		f(Convert(v))
 	}
 }
 
-func CallExcept(except interface{}, f func(r *Caught)) {
+func CallExcept(except interface{}, f func(r Caught)) {
 	if v := recover(); v != nil && except != v {
 		f(Convert(v))
 	}
 }
 
 func Log() {
-	Call(func(r *Caught) {
+	Call(func(r Caught) {
 		kklogger.ErrorJ("panic.Log", r)
 	})
 }
 
 func LogExcept(except interface{}) {
-	CallExcept(except, func(r *Caught) {
+	CallExcept(except, func(r Caught) {
 		kklogger.ErrorJ("panic.Log", r)
 	})
 }
 
-func Catch(main func(), panic func(r *Caught)) {
+func Catch(main func(), panic func(r Caught)) {
 	defer Call(panic)
 	main()
 }
 
-func CatchExcept(main func(), except interface{}, panic func(r *Caught)) {
+func CatchExcept(main func(), except interface{}, panic func(r Caught)) {
 	defer CallExcept(except, panic)
 	main()
 }
 
-type Caught struct {
-	Message         interface{} `json:"message,omitempty"`
-	PanicCallStack  string      `json:"panic_call_stack,omitempty"`
-	GoRoutineStacks string      `json:"go_routine_stacks,omitempty"`
+type Caught interface {
+	String() string
+	Error() string
 }
 
-func (e *Caught) String() string {
+type CaughtImpl struct {
+	Message         interface{} `json:"message,omitempty"`
+	CallStack       string      `json:"call_stack,omitempty"`
+	GoroutineStacks string      `json:"goroutine_stacks,omitempty"`
+}
+
+func (e *CaughtImpl) String() string {
 	bs, _ := json.Marshal(e)
 	return string(bs)
 }
 
-func (e *Caught) Error() string {
+func (e *CaughtImpl) Error() string {
 	bs, _ := json.Marshal(e.Message)
 	return string(bs)
 }
